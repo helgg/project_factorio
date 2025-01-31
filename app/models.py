72bb81310583975c9136c261b586c1c02c459753
helgg <heldergg@gmail.com>
2025-01-31 01:16:28 -0300

    ajustes de models e views para herdar informações da User.

--- app/models.py
+++ app/models.py
@@ -11,12 +11,12 @@ class Blog(models.Model):
     views = models.PositiveIntegerField(default=0)
     unique_views = models.PositiveIntegerField(default=0)
     anonymous_views = models.PositiveIntegerField(default=0)
-    autor = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     is_anonymous = models.BooleanField(default=False)
     thumbnail = models.ImageField(upload_to='thumbnails/', null=True, blank=True)  
 
     def __str__(self):
-        return self.title + ' - ' + str(self.autor)
+        return self.title + ' - ' + str(self.author)
 
     class Meta:
         db_table = "Blog"
@@ -36,19 +36,16 @@ class BlogView(models.Model):
 
 
 class Profile(models.Model):
-    # TODO: Como garantir que ao criar um usuario, um profile seja criado?
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
     bio_details = models.TextField()
     profile_photo = models.ImageField(upload_to='profiles/', null=True, blank=True) 
 
     @property
     def blog_count(self):
-        return Blog.objects.filter(autor=self.user).count()
+        return Blog.objects.filter(author=self.user).count()
     
     def __str__(self):
-         return f"{self.first_name} {self.last_name}"
+        return f"{self.user.first_name} {self.user.last_name}"
     
     class Meta:
         db_table = "Profile"
--- app/views.py
+++ app/views.py
@@ -2,27 +2,44 @@
 import datetime
 
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
 from .models import Blog, BlogView, Profile
 from .forms import BlogForm
 
 # renderiza a home
 def home(request):
+    try:
+        user = Profile.objects.get(user=request.user.id)
+    except Exception:
+        user = 'sem usuario logado'
+
     now = datetime.datetime.now()
     context = {}
+    context['profile'] = user
     context['complete_date'] = now.strftime("%c")
     return render(request, 'home.html', context)
 
 # renderiza o catalogo de blogs (postagens)
 def blogs(request):
+    try:
+        user = Profile.objects.get(user=request.user.id)
+    except Exception:
+        user = 'sem usuario logado'
 
     posts = Blog.objects.all().order_by('-create_at')  
     context = {}
+    context['profile'] = user
     context['posts'] = posts
     return render(request, 'blogs.html', context)
 
 # renderiza o form de postagem
 def make_a_post(request):
+    try:
+        user = Profile.objects.get(user=request.user.id)
+    except Exception:
+        user = 'sem usuario logado'
     context = {}
+    context['profile'] = user
     context['forms'] = BlogForm()
     return render(request, 'post_article.html', context)
 
@@ -45,12 +62,17 @@ def blog_article(request, pk):
 
     blog.views = blog.unique_views + blog.anonymous_views
     blog.save()
+    try:
+        user = Profile.objects.get(user=request.user.id)
+    except Exception:
+        user = 'sem usuario logado'
     context = {}
+    context['profile'] = user
     context['post'] = blog
     return render(request, 'blog_post.html', context)
 
 
-def blog_register(request):
+def blog_register(request):    
     form = BlogForm(request.POST, request.FILES)
     if request.method == 'POST':
         if form.is_valid():
@@ -59,24 +81,28 @@ def blog_register(request):
                 form.is_anonymous = True
             else:
                 form.is_anonymous = False
-            form.autor = request.user
+            form.author = request.user
             form.save()            
     return redirect('app:blogs')
 
 
 def profile(request):
-    user = Profile.objects.get(user=request.user)
-    blogs = Blog.objects.filter(autor=request.user.id)
+    try:
+        user = Profile.objects.get(user=request.user.id)
+    except Exception:
+        user = 'sem usuario logado'
+    blogs = Blog.objects.filter(author=request.user.id)
     context = {}
     context['blog_edit'] = blogs
-    context['user'] = user
+    context['profile'] = user
     # TODO: Caso nao logado redirecinar para login
+    
     return render(request, 'profile.html', context)
 
 
 def edit_blog(request,pk):
 
-    blog = Blog.objects.get(autor=request.user.id, id=pk)
+    blog = Blog.objects.get(author=request.user.id, id=pk)
     form = BlogForm(data=request.POST or None, 
                     files=request.FILES or None,
                     instance=blog)
@@ -91,7 +117,12 @@ def edit_blog(request,pk):
             form.save()  
             return redirect('app:article', pk=blog.id)
         
+    try:
+        user = Profile.objects.get(user=request.user.id)
+    except Exception:
+        user = 'sem usuario logado'
     context = {}
+    context['profile'] = user
     context['forms'] = form
     context['blog'] = blog
   
